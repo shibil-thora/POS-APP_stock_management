a43d4a1e33ddd560c67facf37b7ed70c9a399e02
shibil-thora <shibilallakkat123@gmail.com>
2024-07-22 23:49:23 +0530

    added a part of model

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model): 
+    name = models.CharField(max_length=1000) 
+    product_code = models.CharField(max_length=50) 
+    price = models.DecimalField() # there is something more to add
+    stock = models.IntegerField() 
+    image = models.ImageField() 
+
+
+#sale model   
+#purchase model 
+
